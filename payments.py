2f3a0fd5dd7169743b76adabcaa7333f38e1c3c2
encrypted69-code <iamgreedy6@gmail.com>
2025-07-12 14:10:49 +0530

    Update payments.py

--- payments.py
+++ payments.py
@@ -1,1 +1,8 @@
+def request_withdrawal(users, withdrawals, user_id, amount, upi_id):
+    withdrawals.insert_one({
+        "user_id": user_id,
+        "amount": amount,
+        "upi_id": upi_id,
+        "status": "pending"
+    })
 
